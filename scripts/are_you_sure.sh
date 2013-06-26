881214813e8358825360fe0dddbc2b97661e0e20
gloriajw <strangest@comcast.net>
2013-06-26 11:56:29 -0400

    latest

---
+++ scripts/are_you_sure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+    echo "Are you sure?"
+    read i
+done
--- scripts/io.sh
+++ scripts/io.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+ls -l | grep 'io'    # ls stdout is piped to grep stdin.
+
+ls -l > newfile.txt  # send stdout to a file
+
+echo "~~~~~~~~~~~~~~~~~~~~~" >> newfile.txt
+
+ls -l >> newfile.txt # append to file.
+
+echo "Contents of newfile.txt now: >>>>>>>>>>>>>>" && cat newfile.txt && echo "<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+# Creates an empty file. You'll see error, since in a terminal, stderr goes to stdout by default.
+echo -n "Expected error: " && ls -l nonexistent_file.txt > a.txt  
+
+ls -l nonexistent_file.txt 2> a.err  # redirects stderr
+
+cat a.err > a.txt 2>&1 # 1> is implied.
+cat a.err 1>> a.txt 2>&1 
+cat a.err &>> a.txt # same as above
+
+echo "Contents of a.txt now, 1: >>>>>>>>>>>>>>" && cat a.txt && echo "<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+cat nonexistent.txt >> a.txt 2>&1
+
+echo "Contents of a.txt now, 2: >>>>>>>>>>>>>>" && cat a.txt && echo "<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+rm -rf a.txt a.err newfile.txt
